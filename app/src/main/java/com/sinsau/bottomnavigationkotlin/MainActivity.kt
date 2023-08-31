0ea3f54c4e874c64a56bc62c80674739167f1a7c
sinsau182 <91497861+sinsau182@users.noreply.github.com>
2023-08-31 23:28:07 +0530

    Initial commit

---
+++ app/src/main/java/com/sinsau/bottomnavigationkotlin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.sinsau.bottomnavigationkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val fragmentContainer = findViewById<View>(R.id.fragmentContainer)
+
+        bottomNav.setupWithNavController(fragmentContainer.findNavController())
+    }
+}
